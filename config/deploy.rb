7878723d6bdc2e78cbec94cc1802a359020481af
Andrew Porter <aporter@rapidturtle.com>
2011-11-17 20:06:08 -0700

    Initial import

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+default_run_options[:pty]   = true
+ssh_options[:forward_agent] = true
+
+set :rvm_ruby_string, "1.9.2@twincrest"
+
+# repository elsewhere
+set :scm,        :git
+set :repository, "git@github.com:rapidturtle/com.twincresttech.www.git"
+set :deploy_via, :remote_cache
+
+# user settings
+set :user,     "deploy"
+set :use_sudo, false
+
+# application details
+set :application, "com.twincresttech.www"
+
+set :domain,    "ve.eyequeue.us"
+set :deploy_to, "/home/deploy/#{application}"
+set :rails_env, "production"
+
+role :app, "#{domain}"
+role :web, "#{domain}"
+role :db,  "#{domain}", :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  
+  namespace :config do
+    desc "Create symlink to shared files and folders on each release."
+    task :symlink do
+      # run "ln -nfs #{shared_path}/config/aws.yml #{release_path}/config/aws.yml"
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+  end
+  
+  after "deploy:update_code", "deploy:config:symlink"
+  before "assets:precompile", "deploy:config:symlink"
+end
