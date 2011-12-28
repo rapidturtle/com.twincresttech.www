7c2f8229856327337d2f93382d2d3e457f1243cd
Andrew Porter <aporter@rapidturtle.com>
2011-12-28 10:41:46 -0800

    Migrate manufacturers

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111228002406) do
+
+  create_table "manufacturers", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "web_site"
+    t.string   "support_site"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
