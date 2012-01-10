6841bde71c610a818bce08c73abe5289549f77b6
Andrew Porter <aporter@rapidturtle.com>
2012-01-10 04:50:03 -0700

    Set up basic authentication for staging server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :authenticate_if_staging
+  
 private
 
   def authenticate!
@@ -18,5 +20,13 @@ private
 
   def store_location
     session[:return_to] = request.fullpath
+  end
+  
+  def authenticate_if_staging
+    if request.domain == "rapidturtle.com"
+      authenticate_or_request_with_http_basic do |name, password|
+        name == 'twincrest' && password == 'twincrest'
+      end
+    end
   end
 end
