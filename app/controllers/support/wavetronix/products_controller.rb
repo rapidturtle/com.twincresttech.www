da163e4ae87a6752d3fa257033771457306956a6
Andrew Porter <aporter@rapidturtle.com>
2012-01-23 00:06:42 -0700

    Group downloads by type and sort

--- app/controllers/support/wavetronix/products_controller.rb
+++ app/controllers/support/wavetronix/products_controller.rb
@@ -3,6 +3,7 @@ module Support
     class ProductsController < BaseController
       def show
         @product = Product.find(params[:id], params: { product_line_id: params[:product_line_id] })
+        @downloads = @product.downloads.group_by(&:download_type_id)
         @download_types = DownloadType.all
       end
 
