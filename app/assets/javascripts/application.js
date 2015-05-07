572126e1b8e4ed37d97d6a8dd73caaf76e7ab058
Andrew Porter <aporter@rapidturtle.com>
2015-05-07 12:56:32 -0500

    Fix next round of Hound issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at
 // the bottom of the compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives)
+// Read Sprockets README
+// (https://github.com/rails/sprockets#sprockets-directives)
 // for details about supported directives.
 //
 //= require jquery
