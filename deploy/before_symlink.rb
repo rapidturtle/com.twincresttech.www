6b5b10c5bfa3b4610e70c10fb22cd58be7c876e6
Andrew Porter <andrew.porter@wavetronix.com>
2015-08-13 15:45:53 -0600

    Update precompile assets command
    I’m getting an error every time I try to deploy the app on Opsworks.
    I’ll try this to see if it works.…

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,9 +1,1 @@
-rails_env = new_resource.environment["RAILS_ENV"]
-
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-
-execute "rake assets:precompile" do
-  cwd release_path
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
-end
+run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
