e85fd09c8f8752971b57156dbbd2767e24d8f02c
Andrew Porter <andrew.porter@wavetronix.com>
2015-08-14 21:08:50 -0600

    Skip asset precompile
    Let’s see if it will deploy this time.…

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,9 +1,9 @@
-rails_env = new_resource.environment["RAILS_ENV"]
-
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-
-execute "rake assets:precompile" do
-  cwd release_path
-  command "bundle exec rake assets:precompile 2>&1"
-  environment "RAILS_ENV" => rails_env
-end
+# rails_env = new_resource.environment["RAILS_ENV"]
+#
+# Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+#
+# execute "rake assets:precompile" do
+#   cwd release_path
+#   command "bundle exec rake assets:precompile 2>&1"
+#   environment "RAILS_ENV" => rails_env
+# end
