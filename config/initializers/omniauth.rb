88d852fd20217f1fff3c5ffe5bd68b15d336b699
Andrew Porter <andrew.porter@wavetronix.com>
2015-08-19 20:29:27 -0600

    Reset OmniAuth configuration for CloudFront updates
    I went into the CloudFront distribution and turned on cookies and query
    strings. I should have thought of this sooner. Let’s see if it solves
    the problem.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, {
     name: "google",
     scope: "email, profile",
-    prompt: "select_account",
-    provider_ignores_state: true
+    prompt: "select_account"
   }
 end
