abcde0600b06b03091c34d2840258b4abe56a5de
Andrew Porter <partydrone@me.com>
2017-02-11 10:27:45 -0700

    Create initial Angular 2 app

--- app/assets/javascripts/manufacturers.coffee
+++
@@ -1,5 +0,0 @@
-jQuery ->
-  $("#manufacturers").sortable
-    handle: ".handle"
-    update: ->
-      $.post($(@).data("update-path"), $(@).sortable("serialize"))
--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Load environment variables
-variables = File.join(Rails.root, "config", "variables.rb")
-load(variables) if File.exists?(variables)
-
-# Initialize the Rails application.
-Rails.application.initialize!
