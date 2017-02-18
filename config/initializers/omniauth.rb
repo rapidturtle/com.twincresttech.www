d0bfa4f06eb07429bfb543174773edc073b6cbf1
Andrew Porter <partydrone@me.com>
2017-02-18 01:19:56 -0700

    Deploy first distribution of Angular app

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, {
-    name: "google",
-    scope: "email, profile",
-    prompt: "select_account"
-  }
-end
--- db/migrate/20150814051721_add_logo_content_type_to_manufacturers.rb
+++
@@ -1,5 +0,0 @@
-class AddLogoContentTypeToManufacturers < ActiveRecord::Migration
-  def change
-    add_column :manufacturers, :logo_content_type, :string
-  end
-end
