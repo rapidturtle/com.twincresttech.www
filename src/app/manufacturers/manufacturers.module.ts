707ac23b62e085bfc6fbe2ace5f6f74a142e8447
Andrew Porter <partydrone@icloud.com>
2019-09-07 21:11:11 -0600

    Finish manufacturers

--- src/app/manufacturers/manufacturers.module.ts
+++ src/app/manufacturers/manufacturers.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ManufacturersComponent } from './manufacturers.component';
-
-
+import { SharedModule } from '../shared/shared.module';
+import { ManufacturersService } from './manufacturers.service';
 
 @NgModule({
   declarations: [ManufacturersComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
-  exports: [ManufacturersComponent]
+  exports: [ManufacturersComponent],
+  providers: [ManufacturersService]
 })
 export class ManufacturersModule { }
